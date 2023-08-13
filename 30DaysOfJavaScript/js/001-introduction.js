9a03db9fb635ad952a4bb2862dc980df611474a5
Erdinc <erdincozdemir1812@gmail.com>
2023-08-13 14:46:31 +0300

    js 1 introduction

---
+++ 30DaysOfJavaScript/js/001-introduction.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+
+// console.log(param1, param2, param3); //Yanlış Kullanım
+
+console.log("param1", "param2", "param3");
+
+//"hello world" yazar
+console.log("Hello, World!");
+
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log(`Hello, World!`);
+
+console.log(2 + 3); // Toplama
+console.log(3 - 2); // Çıkarma
+console.log(2 * 3); // Çarpma
+console.log(3 / 2); // Bölme
+console.log(3 % 2); // Mod Alma
+console.log(3 ** 2); // ;Üs Alma 3 ** 2 == 3 * 3
