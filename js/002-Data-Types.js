d98450c9985d85ed4e562ec775d002262d3f6805
Erdinc <erdincozdemir1812@gmail.com>
2023-08-13 17:15:58 +0300

    JS 2 DATA TYPE 2

--- js/002-Data-Types.js
+++ js/002-Data-Types.js
@@ -1,13 +1,52 @@
-let word = 'JavaScript'
-word[0] = 'Y'
-console.log(word)
+// let word = "JavaScript";
+// word[0] = "Y";
+// console.log(word);
 
-let numOne = 3
-let numTwo = 3
+// let numOne = 3;
+// let numTwo = 3;
 
-console.log(numOne == numTwo)
+// console.log(numOne == numTwo);
 
-let js = 'JavaScript'
-let py = 'Python'
+// let js = "JavaScript";
+// let py = "Python";
 
-console.log("js == py : "+(js == py))
+// console.log("js == py : " + (js == py));
+
+// let lightOn = true;
+// let lightOff = false;
+
+// console.log(lightOff == lightOn);
+
+// let nums = [1, "erdinc", true, null, undefined, [1, 2, 3]];
+
+// nums[0];
+// nums[1];
+// nums[2];
+// console.log(nums);
+// console.log(nums[0], nums[1], nums[2]);
+
+// let nums1 = [1, 2, 3]
+// let nums2 = [1, 2, 3]
+// console.log(nums1 == nums2 )//false
+// let nums2 = nums1
+// console.log(nums1 == nums2)
+
+// let age = 31
+// const gravity = 9.81 //değişmeyen değer
+// let mass = 82
+// const PI = 3.14
+// const boilingPoint = 100
+// const bodyTemp = 37
+
+// console.log (age, gravity, mass, PI, boilingPoint, bodyTemp )
+
+const PI = Math.PI;
+console.log(PI);
+
+console.log(Math.round(PI));
+console.log(Math.floor(PI));
+console.log(Math.ceil(PI));
+
+console.log(Math.min(-5, 3, 20, 4, 5, 10)) // En küçük sayıyı bulmaya yaradığı için sonuç -5 döner
+
+console.log(Math.max(-5, 3, 20, 4, 5, 10)) // En büyük sayıyı bulmaya yaradığı için sonuç 20 döner
