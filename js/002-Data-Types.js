8fa1babe244144ff605948a869a455654f5808ad
Erdinc <erdincozdemir1812@gmail.com>
2023-08-13 16:43:21 +0300

    JS 2 DATA TYPE

---
+++ js/002-Data-Types.js
@@ -0,0 +1,13 @@
+let word = 'JavaScript'
+word[0] = 'Y'
+console.log(word)
+
+let numOne = 3
+let numTwo = 3
+
+console.log(numOne == numTwo)
+
+let js = 'JavaScript'
+let py = 'Python'
+
+console.log("js == py : "+(js == py))
