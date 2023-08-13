200fbebae74963a4a83d73a66990fa7b057d5340
Erdinc <erdincozdemir1812@gmail.com>
2023-08-13 21:49:21 +0300

    js 2 data type 4

--- js/002-Data-Types.js
+++ js/002-Data-Types.js
@@ -51,13 +51,33 @@
 
 // console.log(Math.max(-5, 3, 20, 4, 5, 10)) // En büyük sayıyı bulmaya yaradığı için sonuç 20 döner
 
-const num = Math.round(Math.random () * 11) // 0 ile 10 arasında rastgele sayı oluşturur
-console.log(num)
+// const num = Math.round(Math.random() * 11); // 0 ile 10 arasında rastgele sayı oluşturur
+// console.log(num);
 
-let randomNum = Math.random()         // 0 ile 0.999 arasında oluşturur
-let numBtnZeroAndTen = randomNum * 11
+// let randomNum = Math.random(); // 0 ile 0.999 arasında oluşturur
+// let numBtnZeroAndTen = randomNum * 11;
 
-console.log(numBtnZeroAndTen)         // Sonuç : minimum  0 ve maksimum 10.99
+// console.log(numBtnZeroAndTen); // Sonuç : minimum  0 ve maksimum 10.99
 
-let randomNumRoundToRound = Math.round (numBtnZeroAndTen) // yuvarlama yapıyor
-console.log(randomNumRoundToRound)    // Sonuç 0 ile  10 arasında çıkar
+// let randomNumRoundToRound = Math.round(numBtnZeroAndTen); // yuvarlama yapıyor
+// console.log(randomNumRoundToRound); // Sonuç 0 ile  10 arasında çıkar
+
+// let name1 = "Erdinc";
+// let surname = "Ozdemir";
+// let space = " ";
+
+// let fullName = name1 + space + surname;
+// console.log(fullName);
+
+// const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+// I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+// Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+// In the end of 2019, I was thinking to expand my teaching and to reach \
+// to global audience and I started a Python challenge from November 20 - December 19.\
+// It was one of the most rewarding and inspiring experience.\
+// Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+// I hope you are enjoying too."
+// console.log(paragraph)
+
+// let fullName = `${name1} ${surname}`;
+// console.log(fullName);
