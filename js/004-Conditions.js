766f00dd389e2b8e8a87c2249925b7944b391fe1
Erdinc <erdincozdemir1812@gmail.com>
2023-08-16 16:08:22 +0300

    js 4 Conditions if else if else

--- js/004-Conditions.js
+++ js/004-Conditions.js
@@ -10,29 +10,52 @@
 // }
 
 // If else
-let isRainning = false;
-if (isRainning) {
-  console.log("Remember to take your rain coat");
+// let isRainning = false;
+// if (isRainning) {
+//   console.log("Remember to take your rain coat");
+// } else {
+//   console.log("Hava güneşli, şemsiye iptal !!");
+// }
+
+// let result = prompt("2+5 kaç yapar?", "2+5");
+// if (result === "7") {
+//   console.log("Cevabı bildin.");
+// } else {
+//   console.log("Hatalı cevabı verdin, doğru cevap 7");
+// }
+
+// let num1 = 3;
+// if (num1 > 0) {
+//   console.log(`${num1} is a positive number`);
+// } else {
+//   console.log(`${num1} is a negative number`);
+// }
+// let num2 = -3;
+// if (num2 > 0) {
+//   console.log(`${num2} is a positive number`);
+// } else {
+//   console.log(`${num2} is a negative number`);
+// }
+
+// if else if else
+let a = prompt("Lütfen bir sayı giriniz");
+if (a > 0) {
+  console.log(`${a} is a positive number`);
+} else if (a == 0) {
+  console.log(`${a} is zero`);
+} else if (a < 0) {
+  console.log(`${a} is a negative number `);
 } else {
-  console.log("Hava güneşli, şemsiye iptal !!");
+  console.log(`${a} is not a number`);
 }
 
-let result = prompt("2+5 kaç yapar?", "2+5");
-if (result === "7") {
-  console.log("Cevabı bildin.");
+let weather = "sunny";
+if (weather === "rainy") {
+  console.log("You need a rain coat.");
+} else if (weather === "cloudy") {
+  console.log("It might be cold, you need a jacket.");
+} else if (weather === "sunny") {
+  console.log("Go out freely.");
 } else {
-  console.log("Hatalı cevabı verdin, doğru cevap 7");
-}
-
-let num1 = 3;
-if (num1 > 0) {
-  console.log(`${num1} is a positive number`);
-} else {
-  console.log(`${num1} is a negative number`);
-}
-let num2 = -3;
-if (num2 > 0) {
-  console.log(`${num2} is a positive number`);
-} else {
-  console.log(`${num2} is a negative number`);
+  console.log("No need for rain coat.");
 }
