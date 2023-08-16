1b9bebb9f7f2bdf439235ce598d7ecdafdd6b362
Erdinc <erdincozdemir1812@gmail.com>
2023-08-16 13:08:40 +0300

    js 4 conditions if

---
+++ js/004-Conditions.js
@@ -0,0 +1,10 @@
+//if
+let num = 3;
+if (num > 0) {
+    console.log(`${num} is a positive number`)
+}
+
+let isRainning = true
+if (isRainning) {
+    console.log('Remember to take your rain coat')
+}
