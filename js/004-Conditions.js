54bcb203e2bdce0b35006082f54fcf4e28ab1ef9
Erdinc <erdincozdemir1812@gmail.com>
2023-08-16 13:33:02 +0300

    js 4 Conditions if else

--- js/004-Conditions.js
+++ js/004-Conditions.js
@@ -1,10 +1,38 @@
 //if
-let num = 3;
-if (num > 0) {
-    console.log(`${num} is a positive number`)
+// let num = 3;
+// if (num > 0) {
+//     console.log(`${num} is a positive number`)
+// }
+
+// let isRainning = true
+// if (isRainning) {
+//     console.log('Remember to take your rain coat')
+// }
+
+// If else
+let isRainning = false;
+if (isRainning) {
+  console.log("Remember to take your rain coat");
+} else {
+  console.log("Hava güneşli, şemsiye iptal !!");
 }
 
-let isRainning = true
-if (isRainning) {
-    console.log('Remember to take your rain coat')
+let result = prompt("2+5 kaç yapar?", "2+5");
+if (result === "7") {
+  console.log("Cevabı bildin.");
+} else {
+  console.log("Hatalı cevabı verdin, doğru cevap 7");
+}
+
+let num1 = 3;
+if (num1 > 0) {
+  console.log(`${num1} is a positive number`);
+} else {
+  console.log(`${num1} is a negative number`);
+}
+let num2 = -3;
+if (num2 > 0) {
+  console.log(`${num2} is a positive number`);
+} else {
+  console.log(`${num2} is a negative number`);
 }
