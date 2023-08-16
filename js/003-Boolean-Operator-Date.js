e53009cf3c2e38f774fc59ace4e052511e31bf62
Erdinc <erdincozdemir1812@gmail.com>
2023-08-16 06:39:20 +0300

    js 3 Boolean Operator Date Exercise 2

--- js/003-Boolean-Operator-Date.js
+++ js/003-Boolean-Operator-Date.js
@@ -63,7 +63,6 @@
 // console.log(date);
 // date.getFullYear()
 
-
 //Okunabilir Saat Göstergesi
 // const date = new Date();
 // let year = date.getFullYear(),
@@ -209,3 +208,159 @@
 // console.log(humanReadableDate)
 
 // Exercise lvl 2
+
+// 2.1
+let taban = prompt("Üçgenin Taban Değerini Giriniz!");
+let yükseklik = prompt("Üçgenin Yükseklik Değerini Giriniz");
+let result = taban * yükseklik * 0.5;
+alert(`Üçgenin alanı : ${result}cm^2`);
+
+// 2.2
+let firstPeriper = prompt("üçgenin ilk kenar değerini giriniz");
+let twoPeriper = prompt("üçgenin ikinci kenar değerini giriniz");
+let threePeriper = prompt("üçgenin üçüncü kenarını giriniz");
+
+let first = firstPeriper;
+let two = twoPeriper;
+let three = threePeriper;
+
+const convertOne = Number(first);
+const convertTwo = Number(two);
+const convertThree = Number(three);
+
+const perimeter = convertOne + convertTwo + convertThree;
+alert("Üçgenin Çevresi : " + perimeter + " cm");
+
+// 2.3
+const length = prompt("Dikdörtgenin Taban Değerini Giriniz");
+const width = prompt("Dikdörtgenin Yükseklik değerini giriniz");
+
+const areaFormul = Number(length) * Number(width);
+console.log(`Dikdörtgenin alanı ${areaFormul} cm^2`);
+const rectangelFormul = 2 * (Number(length) + Number(width));
+console.log(`Dikdörtgenin çevresi ${rectangelFormul} cm`);
+
+// 2.4
+while (true) {
+  var userInput = prompt("Lütfen Dairenin Yarıçapını giriniz:");
+
+  // Kullanıcının "iptal" tuşuna bastığını kontrol et
+  if (userInput === null) {
+    alert("İşlem iptal edildi.");
+    break;
+  }
+
+  // Girilen değeri sayıya çevirme deneme
+  var radius = parseFloat(userInput);
+
+  // Sayı çevirme işlemi başarılı ise devam et
+  if (!isNaN(radius)) {
+    console.log("Girilen sayı: " + radius);
+    // Burada istediğiniz işlemi gerçekleştirebilirsiniz
+    break;
+  } else {
+    alert("Geçerli bir sayı girmediniz. Lütfen tekrar deneyin.");
+  }
+}
+
+const PI = 3.14;
+const circleArea = PI * radius * radius;
+console.log(`Dairenin alanı ${circleArea}`);
+
+// 2.9
+const hoursPrompt = prompt("Enter hours");
+const perHoursSalaryPrompt = prompt("Enter rate per hour");
+
+const hours = hoursPrompt;
+const perHoursSalary = perHoursSalaryPrompt;
+
+const salaryFormul = hoursPrompt * perHoursSalaryPrompt;
+alert(salaryFormul);
+
+// 2.10
+const humanName = prompt("Adınızı Giriniz");
+const lengthName = humanName.length;
+alert(lengthName > 7 ? "Adınız Uzun" : "Adınız Kısa");
+
+// 2.11
+let yourFirstName = prompt("Adınızı Gİriniz");
+let yourLastName = prompt("Soyadınızı Giriniz");
+
+let resultLength =
+  yourFirstName.length > yourLastName.length
+    ? "Adınız, soyadınızdan uzundur"
+    : "Adınız, soyadınızdan kısadır";
+
+alert(resultLength);
+
+// 2.12
+let myAge = prompt("Benim Yaşım Kaç");
+let yourAge1 = prompt("Senin Yaşın Kaç");
+
+const age1 = parseFloat(myAge);
+const age2 = parseFloat(yourAge1);
+if (age1 > age2) {
+  let resultAge1 = age1 - age2;
+  console.log(`Senden ${resultAge1} yaş büyüğüm.`);
+} else {
+  let resultAge2 = age2 - age1;
+  console.log(`Senden ${resultAge2} yaş küçüğüm.`);
+}
+
+// 2.13
+let date1 = new Date();
+let nowYear = date1.getFullYear();
+let yourBirthday = prompt("Doğum yılınızı giriniz");
+let yourAge = nowYear - yourBirthday;
+let legalAge = 18;
+alert(yourAge > 18 ? "Ehliyet alabilirsin" : "Ehliyet alamazsın");
+
+//2.14
+const ageCalculatorPrompt = prompt("Enter number of years you live");
+const ageCalculate = ageCalculatorPrompt;
+const secondFormul = ageCalculate * 365 * 24 * 60 * 60;
+
+if (ageCalculate > 100) {
+  alert("As a rule, you cannot live more than 100 years");
+} else {
+  alert(`You lived ${secondFormul} seconds.`);
+}
+
+// 2.15
+const date = new Date();
+let year = date.getFullYear(),
+  month = date.getMonth(),
+  day = date.getDate(),
+  hour = date.getHours(),
+  minute = date.getMinutes(),
+  seconds = date.getSeconds();
+dayNumber = date.getDay();
+let months = [
+  "Ocak",
+  "Şubat",
+  "Mart",
+  "Nisan",
+  "Mayıs",
+  "Haziran",
+  "Temmuz",
+  "Ağustos",
+  "Eylül",
+  "Ekim",
+  "Kasım",
+  "Aralık",
+];
+let days = [
+  "Pazar",
+  "Pazartesi",
+  "Salı",
+  "Çarşamba",
+  "Perşembe",
+  "Cuma",
+  "Cumartesi",
+  "Pazar",
+];
+
+// örnek time: 15 Ağustos 2023, Çarşamba, 21:21:21
+
+let humanReadableDate = `${day} ${months[month]} ${year}, ${days[dayNumber]}, ${hour}:${minute}:${seconds} `;
+console.log(humanReadableDate);
