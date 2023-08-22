d2659d3756a3f3ebd6cb6912bd09bee3ed1eec54
Erdinc <erdincozdemir1812@gmail.com>
2023-08-22 11:15:11 +0300

    js 8 objects 3

--- js/008-objects.js
+++ js/008-objects.js
@@ -49,50 +49,93 @@
 //     isMarried: true
 //   }
 //   console.log(person)
+// const person = {
+//   firstName: "Erdinc",
+//   lastName: "Ozdemir",
+//   age: 250,
+//   country: "Turkey",
+//   city: "Istanbul",
+//   skills: [
+//     "HTML",
+//     "CSS",
+//     "JavaScript",
+//     "React",
+//     "Node",
+//     "MongoDB",
+//     "Java",
+//     "SQL",
+//   ],
+//   getFullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+// const formatter = new Intl. ("en", {
+//   style: "long",
+//   type: "conjuction",
+// });
+
+// person.nationality = "Turkısh";
+// person.country = "Turkey";
+// person.title = "Engineer";
+// person.skills.push("Meteor");
+// person.skills.push("SasS");
+// person.isMarried = true;
+
+// person.getPersonInfo = function () {
+// let skillsWithoutLastSkill = this.skills
+//   .splice(0, this.skills.length - 1)
+//   .join(', ')
+// let lastSkill = this.skills.at(-1)
+
+// let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`
+//   let skills = formatter.format(this.skills);
+//   let fullName = this.getFullName();
+//   let statement = `${fullName} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${skills}.`;
+//   return statement;
+// };
+// console.log(person);
+// person.skills.push("Python");
+// console.log(person.getPersonInfo());
+
 const person = {
-  firstName: "Erdinc",
-  lastName: "Ozdemir",
+  firstName: "Asabeneh",
   age: 250,
-  country: "Turkey",
-  city: "Istanbul",
-  skills: [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Node",
-    "MongoDB",
-    "Java",
-    "SQL",
-  ],
-  getFullName: function () {
-    return `${this.firstName} ${this.lastName}`;
+  country: "Finland",
+  city: "Helsinki",
+  skills: ["HTML", "CSS", "JS"],
+  title: "teacher",
+  address: {
+    street: "Heitamienkatu 16",
+    pobox: 2002,
+    city: "Helsinki",
+  },
+  getPersonInfo: function () {
+    return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`;
   },
 };
-const formatter = new Intl. ("en", {
-  style: "long",
-  type: "conjuction",
-});
 
-person.nationality = "Turkısh";
-person.country = "Turkey";
-person.title = "Engineer";
-person.skills.push("Meteor");
-person.skills.push("SasS");
-person.isMarried = true;
+//Object methods: Object.assign, Object.keys, Object.values, Object.entries
+//hasOwnProperty
 
-person.getPersonInfo = function () {
-  // let skillsWithoutLastSkill = this.skills
-  //   .splice(0, this.skills.length - 1)
-  //   .join(', ')
-  // let lastSkill = this.skills.at(-1)
+// const copyPerson = Object.assign({}, person)
+let copyPerson = { ...person };
+Object.freeze(copyPerson);
 
-  // let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`
-  let skills = formatter.format(this.skills);
-  let fullName = this.getFullName();
-  let statement = `${fullName} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${skills}.`;
-  return statement;
-};
+copyPerson.firstName = "Erdinc";
+copyPerson.age = "30";
 console.log(person);
-person.skills.push("Python");
-console.log(person.getPersonInfo());
+console.log(copyPerson);
+
+console.log(Object.keys(person));
+console.log(Object.values(person));
+// console.log(Object.entries(person));
+for (let [key, val] of Object.entries(person)) {
+  console.log(key, val);
+}
+
+console.log(person.hasOwnProperty("country"));
+
+// obje dondurma freeze
+
+// Object.freeze(copyPerson);
+// object.seal (copyPerson); object e bişey ekleyemeden mevcut keyleri değiştirmek için kullanılır.)
