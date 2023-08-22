d027c3c7d367f8582b773583039081dec5ae46a7
Erdinc <erdincozdemir1812@gmail.com>
2023-08-22 23:35:09 +0300

    js 10 setAndMaps 4

--- js/10-setAndMaps.js
+++ js/10-setAndMaps.js
@@ -1,17 +1,17 @@
 const names = ["Erdinc", "Hava", "Ismail", "Hacer", "Erdinc", "Hava"];
 let namesSet = new Set();
 
-// for (let name of names) {
-//   console.log("Ad", name);
-// }
+for (let name of names) {
+  console.log("Ad", name);
+}
 
-// names.forEach((name) => console.log(name));
+names.forEach((name) => console.log(name));
 
-// [...names].map((name) => console.log(name));
+[...names].map((name) => console.log(name));
 
-// names.add("ezgi");
-// names.add("ayse");
-// console.log(names);
+names.add("ezgi");
+names.add("ayse");
+console.log(names);
 
 for (let name of names) {
   namesSet.add(name);
@@ -53,12 +53,38 @@ const numbers2 = [5, 5, 5, 5, 2, 2, 3, 3, 4, 4, 8];
 const setOfNumbers = new Set(numbers2);
 console.log(setOfNumbers);
 
-let a = [1, 2, 3, 4, 5];
-let b = [6, 7, 7, 8, 9];
+// let a = [1, 2, 3, 4, 5];
+// let b = [6, 7, 7, 8, 9];
 
-let c = [...a, ...b];
+// let c = [...a, ...b];
 
+// let C = new Set(c);
+// console.log(C);
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 8, 7, 6, 5, 4, 3];
+let b = [3, 4, 5, 6, 7];
+let A = new Set(a);
+let B = new Set(b);
+let c = a.filter((num) => B.has(num));
 let C = new Set(c);
 console.log(C);
 
+const map = new Map();
 
+map.set("name", "erdinc");
+map.set(1, "Hava");
+console.log(map);
+
+const obj = {};
+obj["name"] = "Erdinc";
+obj[1] = "hava";
+console.log(obj[1]);
+
+const erdinc = {
+  name: "erdinc",
+};
+const hava = {
+  name: "hava",
+};
+const map = new Map([["name", "erdinc"]]);
+console.log(map.get("name"));
