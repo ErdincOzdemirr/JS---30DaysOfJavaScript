89dc2666e5154d628eab71bfb2e479ea5ea04c2a
Erdinc <erdincozdemir1812@gmail.com>
2023-08-22 21:57:37 +0300

    js 10 setAndMaps 3

--- js/10-setAndMaps.js
+++ js/10-setAndMaps.js
@@ -1,4 +1,4 @@
-const names = ["Erdinc", "Hava", "Ismail", "Hacer"];
+const names = ["Erdinc", "Hava", "Ismail", "Hacer", "Erdinc", "Hava"];
 let namesSet = new Set();
 
 // for (let name of names) {
@@ -21,3 +21,44 @@ namesSet.delete("Ahmet");
 console.log(namesSet.has("Ahmet"));
 namesSet.clear();
 console.log(namesSet.has("Erdinc"));
+
+console.log(names);
+console.log(new Set(names));
+console.log(...new Set(names));
+
+const languages = [
+  "English",
+  "Finnish",
+  "English",
+  "French",
+  "Spanish",
+  "English",
+  "French",
+];
+const langSet = new Set(languages);
+
+console.log(langSet);
+console.log(langSet.size);
+
+const counts = [];
+const count = {};
+for (let l of langSet) {
+  const filteredLang = languages.filter((lng) => lng === l);
+  console.log(filteredLang);
+  counts.push({ lang: l, count: filteredLang.length });
+}
+console.log(counts);
+
+const numbers2 = [5, 5, 5, 5, 2, 2, 3, 3, 4, 4, 8];
+const setOfNumbers = new Set(numbers2);
+console.log(setOfNumbers);
+
+let a = [1, 2, 3, 4, 5];
+let b = [6, 7, 7, 8, 9];
+
+let c = [...a, ...b];
+
+let C = new Set(c);
+console.log(C);
+
+
