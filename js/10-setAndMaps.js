0cedb66ace6f48a00ce27731ae74b2d6707f6dc8
Erdinc <erdincozdemir1812@gmail.com>
2023-08-22 21:06:25 +0300

    js 10 setAndMaps 2

--- js/10-setAndMaps.js
+++ js/10-setAndMaps.js
@@ -1,14 +1,23 @@
-let names = new Set(["Erdinc", "Hava", "Ismail", "Hacer"]);
+const names = ["Erdinc", "Hava", "Ismail", "Hacer"];
+let namesSet = new Set();
+
+// for (let name of names) {
+//   console.log("Ad", name);
+// }
+
+// names.forEach((name) => console.log(name));
+
+// [...names].map((name) => console.log(name));
+
+// names.add("ezgi");
+// names.add("ayse");
+// console.log(names);
 
 for (let name of names) {
-  console.log("Ad", name);
+  namesSet.add(name);
 }
-
-names.forEach((name) => console.log(name));
-
-[...names].map((name) => console.log(name));
-
-names.add("ezgi");
-names.add("ayse");
-console.log(names);
-
+namesSet.add("Ahmet");
+namesSet.delete("Ahmet");
+console.log(namesSet.has("Ahmet"));
+namesSet.clear();
+console.log(namesSet.has("Erdinc"));
