f7ebeaee5d7aa2a8ad417ba26608cb742faf76be
Erdinc <erdincozdemir1812@gmail.com>
2023-08-23 09:08:42 +0300

    js 11 spread

--- js/011-DestructingSpreadRest.js
+++ js/011-DestructingSpreadRest.js
@@ -1,1 +1,29 @@
 // By Erdinc Destructing Spread Rest
+const numbers = [10, 12, 14];
+
+const [, , number3] = numbers;
+console.log(number3);
+console.log(numbers[0]);
+
+console.log(numbers[1]);
+
+console.log[numbers[2]];
+console.log(number3);
+
+const names = ["Erdinc", "Brook", "David", "John"];
+let [firstPerson, secondPerson, thirdPerson, fourthPerson] = names;
+
+console.log(firstPerson, secondPerson, thirdPerson, fourthPerson);
+
+const fullStack = [
+  ["HTML", "CSS", "JS", "React"],
+  ["Node", "Express", "MongoDB"],
+  "java",
+  "node.js",
+  "angular",
+];
+const [frontEnd, backEnd, ...otherLanguages] = fullStack;
+
+console.log(frontEnd);
+console.log(backEnd);
+console.log(otherLanguages);
