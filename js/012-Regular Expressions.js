818b53957f5ebc697fc20415f2b5455180bae98e
Erdinc <erdincozdemir1812@gmail.com>
2023-08-25 08:48:23 +0300

    js 12 regular exp 2

--- js/012-Regular Expressions.js
+++ js/012-Regular Expressions.js
@@ -7,11 +7,17 @@ let pattern = "love";
 let flag = "gi";
 let regEx = new RegExp(pattern, flag);
 
-let regEX2 = /love/gi;
-console.log(regEX2);
+// let regEX2 = /love/gi;
+// console.log(regEX2);
 // By Erdinc
+// aranan şey regex // biçiminde yazılır. test sadece olup olmadığını kontrol eder ve true ve false döndürür.
+// match ise index bilgileri dahil döndürür.
+// search ise aranan regex in index inin çevirir. 
 let word = /salak/gi;
 let comment = prompt("yaz");
 if (word.test(comment)) {
   alert("Hayır sen salaksin");
 }
+
+let string = "I love javascript and ı love Hava";
+console.log(string.match(/love/gi));
