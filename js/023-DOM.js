76d6a33ecee1673fb222cd2c8dd7e41a929a62c6
Erdinc <erdincozdemir1812@gmail.com>
2023-09-07 08:00:47 +0300

    js 23 DOM event

--- js/023-DOM.js
+++ js/023-DOM.js
@@ -1,1 +1,24 @@
-// by Erdinç ÖZDEMİR 2
+const button = document.getElementById("btn");
+
+button.addEventListener("click", (e) => {
+  console.log("e. object gösterim : ", e);
+  console.log("e.target neyi getirdi : ", e.target);
+});
+
+function clickHandle(e) {
+  console.log("e : ", e);
+  console.log("e.id : ", e.id);
+  console.log(e.getAttribute("id"));
+}
+
+button.onclick = (e) => {
+  console.log(e);
+};
+
+const clickMe = () => {
+    console.log("Bana tıklandı :D")
+}
+
+button.addEventListener("dblclick", (e) => {
+    console.log("Bana double tıklandı")
+})
