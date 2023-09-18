fd25738bc44caeae0273451c5dde20c6ce2dac1c
Erdinc <erdincozdemir1812@gmail.com>
2023-09-18 07:57:37 +0300

    js 27 toggleApp

--- exercise/JSToDoToggleApp/js.js
+++ exercise/JSToDoToggleApp/js.js
@@ -2,27 +2,40 @@ const buttons = document.querySelectorAll(".btn");
 console.log(buttons);
 
 buttons.forEach((button) => {
-  console.log("button", button);
-//   const button = document.querySelector("#btn");
-// const content = document.getElementById("content");
 
-const content = document.querySelector(button.dataset.target);
-console.log(content);
-button.innerText = button.dataset.hide;
-button.addEventListener("click", () => {
-  console.log(button.dataset);
-  if (content.style.display === "" || content.style.display === "block") {
-    console.log("content sayfada görünür");
-    content.style.display = "none";
-    // butonun text ini göster olarak ayarlanmak
-    // button.innerText = "Göster";
-    button.innerText = button.dataset.show;
-  } else {
-    console.log("content sayfada gizli");
-    content.style.display = "";
-    // butonun text ini gizle olarak ayarlanmak
-    // button.innerText = "Gizle";
-    button.innerText = button.dataset.hide;
-  }
-});
+
+  console.log("button", button);
+  //   const button = document.querySelector("#btn");
+  // const content = document.getElementById("content");
+
+  const content = document.querySelector(button.dataset.target);
+  console.log(content);
+  button.innerText = button.dataset.hide;
+  button.addEventListener("click", () => {
+    console.log(button.dataset);
+    // 1.yol. content'in displayinin block none değişimiyle gizleme
+    if (content.style.display === "" || content.style.display === "block") {
+      console.log("content sayfada görünür");
+      content.style.display = "none";
+      // butonun text ini göster olarak ayarlanmak
+      // button.innerText = "Göster";
+      button.innerText = button.dataset.show;
+    } else {
+      console.log("content sayfada gizli");
+      content.style.display = "";
+      // butonun text ini gizle olarak ayarlanmak
+      // button.innerText = "Gizle";
+      button.innerText = button.dataset.hide;
+    }
+// 2.yol
+// if (content.classList.contains('hide')){
+//     button.innerText = button.dataset.hide
+//     content.classList.remove('.hide')
+
+// } else {
+//     console.log(gizlenmeli)
+//     button.innerText = button.dataset.show
+//     content.classList.add('hide')
+// }
+  });
 });
