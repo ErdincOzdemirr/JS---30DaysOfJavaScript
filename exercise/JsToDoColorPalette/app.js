d042b0cba17e710d6ae0c682acc02b54a6ed8443
Erdinc <erdincozdemir1812@gmail.com>
2023-09-20 07:17:23 +0300

    JsToDoColorPalette

--- exercise/JsToDoColorPalette/app.js
+++ exercise/JsToDoColorPalette/app.js
@@ -11,7 +11,6 @@ const button = document.getElementById("generate-color-btn");
 //   document.body.style.backgroundColor = colors[randomNumber];
 // });
 
-
 // #### 2.colorpalette rgb değerindeki 0-256 arası sayı seçerek yapma
 
 // button.addEventListener("click", () => {
@@ -26,25 +25,43 @@ const button = document.getElementById("generate-color-btn");
 //   document.body.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
 // });
 
-
 // #### 3. Renk değişimini animasyonlu geçiş
 // css de transition komutu eklenir.
 
 // #### 4. Fonksiyon ile renk üretim tekrarından kaçınarak rgb değeri oluşturma
 
+// const generateRandomNumber = (maxNumber) => {
+//     return Math.floor(Math.random() * (maxNumber + 1))
+//     // max number + 1, 0 ve 255 in dahil olduğu istediğimiz için +1
+// }
+
+// button.addEventListener("click", () => {
+//     console.log("tıkladın");
+
+//     const randomNumber1 = generateRandomNumber(255);
+//     const randomNumber2 = generateRandomNumber(255);
+//     const randomNumber3 = generateRandomNumber(255);
+
+//     console.log(randomNumber1, randomNumber2, randomNumber3);
+
+//     document.body.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+//   });
+
+// #### 5. Fonksiyon ile  rgb değeri oluşturma
+
 const generateRandomNumber = (maxNumber) => {
-    return Math.floor(Math.random() * (maxNumber + 1))
-    // max number + 1, 0 ve 255 in dahil olduğu istediğimiz için +1
-}
+  return Math.floor(Math.random() * (maxNumber + 1));
+  // max number + 1, 0 ve 255 in dahil olduğu istediğimiz için +1
+};
 
+const generateRGBColor = () => {
+  return [
+    generateRandomNumber(255),
+    generateRandomNumber(255),
+    generateRandomNumber(255),
+  ].join(", ");
+};
 button.addEventListener("click", () => {
-    console.log("tıkladın");
-
-    const randomNumber1 = generateRandomNumber(255);
-    const randomNumber2 = generateRandomNumber(255);
-    const randomNumber3 = generateRandomNumber(255);
-
-    console.log(randomNumber1, randomNumber2, randomNumber3);
-
-    document.body.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
-  });
+  console.log("tıkladın");
+  document.body.style.backgroundColor = `rgb(${generateRGBColor()})`;
+});
