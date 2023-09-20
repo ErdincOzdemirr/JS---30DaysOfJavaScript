28b3f9f7a55d104409fa3b766f950004141ff364
Erdinc <erdincozdemir1812@gmail.com>
2023-09-20 07:09:15 +0300

    JsToDoColorPalette with function

--- exercise/JsToDoColorPalette/app.js
+++ exercise/JsToDoColorPalette/app.js
@@ -14,20 +14,37 @@ const button = document.getElementById("generate-color-btn");
 
 // #### 2.colorpalette rgb değerindeki 0-256 arası sayı seçerek yapma
 
-button.addEventListener("click", () => {
-  console.log("tıkladın");
+// button.addEventListener("click", () => {
+//   console.log("tıkladın");
 
-  const randomNumber1 = Math.floor(Math.random() * 256);
-  const randomNumber2 = Math.floor(Math.random() * 256);
-  const randomNumber3 = Math.floor(Math.random() * 256);
+//   const randomNumber1 = Math.floor(Math.random() * 256);
+//   const randomNumber2 = Math.floor(Math.random() * 256);
+//   const randomNumber3 = Math.floor(Math.random() * 256);
 
-  console.log(randomNumber1, randomNumber2, randomNumber3);
+//   console.log(randomNumber1, randomNumber2, randomNumber3);
 
-  document.body.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
-});
+//   document.body.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+// });
 
 
 // #### 3. Renk değişimini animasyonlu geçiş
 // css de transition komutu eklenir.
 
 // #### 4. Fonksiyon ile renk üretim tekrarından kaçınarak rgb değeri oluşturma
+
+const generateRandomNumber = (maxNumber) => {
+    return Math.floor(Math.random() * (maxNumber + 1))
+    // max number + 1, 0 ve 255 in dahil olduğu istediğimiz için +1
+}
+
+button.addEventListener("click", () => {
+    console.log("tıkladın");
+
+    const randomNumber1 = generateRandomNumber(255);
+    const randomNumber2 = generateRandomNumber(255);
+    const randomNumber3 = generateRandomNumber(255);
+
+    console.log(randomNumber1, randomNumber2, randomNumber3);
+
+    document.body.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+  });
